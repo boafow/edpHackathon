56f1e321b5b1fe2d267cfa34f75410a9e0163b15
Alice Smith <alice@smith.com>
2023-07-13 12:33:48 -0400

    added schema

--- server/model/model.js
+++ server/model/model.js
@@ -1,2 +1,26 @@
 const mongoose = require('mongoose');
 
+const planetsSchema = new mongoose.Schema(
+    {
+        id: Number,
+        climate: String,
+        surface_water: String,
+        name: String,
+        diameter: String,
+        rotation_period: String,
+        terrain: String,
+        gravity: String,
+        orbital_period: String,
+        population: String
+    }
+);
+
+const Planets = mongoose.model('planets', planetsSchema);
+const Films = mongoose.model('film', filmsSchema);
+const Characters = mongoose.model('character', charactersSchema);
+const Films_characters = mongoose.model('films_characters', filmsCharactersSchema);
+const Films_planets = mongoose.model('films_planets', filmsPlanetsSchema);
+
+module.exports = {
+    Planets, Films, Characters, Films_characters, Films_planets
+} 
